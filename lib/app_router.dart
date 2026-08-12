1ee3642d3c2544b3f09f68be96ac3d5d804550ce
Emmanuelprod <dornuemmanuel3@gmail.com>
2026-08-12 21:07:54 +0100

    Create AppRouter class with route constants

---
+++ lib/app_router.dart
@@ -0,0 +1,8 @@
+class AppRouter {
+  static const String home = '/';
+  static const String search = '/search';
+  static const String notifications = '/notifications';
+  static const String profile = '/profile';
+  static const String login = '/login';
+  static const String signup = '/signup';
+}
